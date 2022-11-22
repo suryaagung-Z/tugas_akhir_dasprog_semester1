b79d3f9871053857ad09705d56cfa53489dadf15
suryaagung-Z <suryaagung118@gmail.com>
2022-11-22 20:56:45 +0700

    repair db.sql

--- db.sql
+++ db.sql
@@ -29,7 +29,7 @@ CREATE TABLE `pendaftaran_bis` (
   `no_telp` bigint NOT NULL,
   `tgl_pemesanan` varchar(128) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
