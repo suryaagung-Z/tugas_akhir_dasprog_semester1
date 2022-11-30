0943e641ae1c13674e0823dd8cb457b720727275
suryaagung-Z <suryaagung118@gmail.com>
2022-11-30 11:59:06 +0700

    tracing repair

--- app/Core.py
+++ app/Core.py
@@ -6,13 +6,15 @@ from plyer import notification
 class Core(Database):
     # Contructor
     def __init__(self):
+        # Inisialisasi contructor parent class / Database
         super().__init__()
     
+    # Awal mula program
     def start(self):
         self.showTable()
         self.nextstep()
 
-    # Function step
+    # Function langkah-langkah
     def nextstep(self):
         _next = input("| TAMBAH | PERBARUI | HAPUS | KELUAR | => ").lower()
         if _next == "tambah":
@@ -32,7 +34,9 @@ class Core(Database):
 
     # Function Tampilkan Semua Data Kedalam Tabel
     def showTable(self):
+        # Membuat tabel pendaftaran_bis jika tabel pendaftaran_bis tidak tersedia
         super().createTable()
+        # Dapatkan semua data didalam tabel pendaftaran_bis
         result = super().getAll()
 
         pt = PrettyTable()
@@ -41,6 +45,7 @@ class Core(Database):
         
         no = 1
         for row in result:
+            # Memasukan semua data tabel pendaftaran_bis kedalam tabel
             pt.add_row([no, row["nama_penumpang"], row["alamat"], row["no_telp"], row["tgl_pemesanan"]])
             no+=1
         print(f"{pt} \n")
@@ -48,18 +53,22 @@ class Core(Database):
     # Function Tambah Data
     def add(self):
         self.heading("TAMBAH DATA")
+        # Menerima input data dari pengguna
         nama     = input("Nama : ").strip()
         alamat   = input("Alamat : ").strip()
         noTelp   = input("No.telp : ").strip()
         tglPesan = input("Tanggal Pemesanan : ").strip()
         
+        # Cek apakah terdapat data kosong, jika true maka gagalkan proses tambah data
         if ((nama=="") or (alamat=="") or (noTelp=="") or (tglPesan=="")):
             self.showNotif("Terdapat Data Kosong")
             self.start()
             return
 
+        # Jalankan proses tambah data dengan memanggil method addData()
         result = super().addData(nama=nama, alamat=alamat, noTelp=noTelp, tglPesan=tglPesan)
 
+        # Memeriksa apakah ada perubahan dari database
         if result.rowcount == 1:
             self.showNotif("Data Berhasil Ditambah")
             self.start()
@@ -68,13 +77,17 @@ class Core(Database):
     # Function Perbarui Data
     def update(self):
         self.heading("PERBARUI")
+        # Menerima input nomor baris yang ingin diperbarui
         rowNum = input("Masukan nomor : ")
+        # Jika yang dimasukan kosong atau bukan angka maka hentikan proses perbarui
         if ((rowNum == "") or (rowNum.isnumeric() == False)):
             self.showNotif("Gagal : Data Tidak Ditemukan")
             self.start()
             return
-
+        
+        # Dapatkan data dari tabel berdasarkan baris yang diinginkan pengguna
         getRow = self.getByRow(rowNum)
+        # Cek apakah data tersedia, jika tidak maka hentikan proses perbarui
         if getRow == None:
             self.showNotif("Gagal : Data Tidak Ditemukan")
             self.start()
@@ -82,23 +95,28 @@ class Core(Database):
 
         self.message("CATATAN : \"ENTER\" jika tidak ingin diperbarui")
         
+        # Menerima input data untuk diperbarui
         nama     = input(f"Nama          : {getRow['nama_penumpang']} -> ").strip()
         alamat   = input(f"Alamat        : {getRow['alamat']} -> ").strip()
         noTelp   = input(f"No.Telp       : {getRow['no_telp']} -> ").strip()
         tglPesan = input(f"Tanggal Pesan : {getRow['tgl_pemesanan']} -> ").strip()
 
+        # Cek apakah data kosong semua, jika true maka hentikan proses perbarui
         if ((nama=="") and (alamat=="") and (noTelp=="") and (tglPesan=="")):
             self.showNotif("Tidak Ada Data Yang Diperbarui")
             self.start()
             return
 
+        # Cek data baru apakah kosong, jika kosong maka timpa dengan data lama
         nama     = nama if (nama != "") else getRow['nama_penumpang']
         alamat   = alamat if (alamat != "") else getRow['alamat']
         noTelp   = int(noTelp) if (noTelp != "") else getRow['no_telp']
         tglPesan = tglPesan if (tglPesan != "") else getRow['tgl_pemesanan']
 
+        # Jalankan proses perbarui data dengan memanggil updateData()
         result = super().updateData(id=getRow['id'], nama=nama, alamat=alamat, noTelp=noTelp, tglPesan=tglPesan)
 
+        # Cek apakah ada perubahan dari database
         if result.rowcount == 1:
             self.showNotif("Data Berhasil Diperbarui")
             self.start()
@@ -107,24 +125,31 @@ class Core(Database):
     # Function Hapus Data
     def delete(self):
         self.heading("HAPUS DATA")
+
+        # Menerima input nomor baris yang ingin dihapus
         rowNum = input("Masukan nomor : ")
+        # Jika yang dimasukan kosong atau bukan angka maka hentikan proses hapus
         if ((rowNum == "") or (rowNum.isnumeric() == False)):
             self.showNotif("Data Tidak Ditemukan")
             self.start()
             return
         
+        # Jalankan proses hapus data dengan memanggil method deleteData()
         result = super().deleteData(rowNum)
+        # Cek apakah data tersedia, jika tidak maka hentikan proses hapus
         if result == None:
             self.showNotif("Data Tidak Ditemukan")
             self.start()
             return
 
+        # Cek apakah ada perubahan dari database
         if result.rowcount == 1:
             self.showNotif("Data Berhasil Dihapus")
             self.start()
         print("\n")
 
 
+    # FITUR
     def heading(self, val):
         print(f"\n~~~~~~~~~~~~~~~~~~~~~~~~~~~{val}~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
 
@@ -137,3 +162,4 @@ class Core(Database):
             message = message,
             timeout = 2
         )
+    # END FITUR
